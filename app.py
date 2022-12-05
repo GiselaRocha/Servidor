4b0c94a4b55fb921e1030b6bafb80a10206ae1fa
GiselaRocha <gisela.ra@culiacan.tecnm.mx>
2022-12-05 10:35:38 -0700

    Servidor local para red neuronal de Elemental

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin  #Para poder atender las peticiones localmente
+from flask import render_template
+from flask import abort, jsonify
+from persontext import predict_personalidad
+from werkzeug.utils import redirect
+import os
+
+app = Flask(__name__)
+
+#Para poder atender las peticiones locales
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+APP_ROOT=os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
+def home():
+    """
+    Página inicial
+    """
+    return "<h1>Bienvenidos a ApiPersonText v0.11<h1>"
+
+#@app.route('/anylink')
+#def anylink():
+#    pass
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+@app.route('/persontext/<texto>',methods=["GET","POST"])
+@cross_origin() #Para poder atender las peticiones locales
+def persontext(texto):
+    datos = {}
+    prediccion = predict_personalidad(texto.lower())
+    datos["presenta_neuroticismo"] = prediccion
+    return jsonify(datos)
+
+
+if __name__ == "__main__":
+   app.run() ##Replaced with below code to run it using waitress 
+   #serve(app, host='0.0.0.0', port=5000, url_scheme='https')
